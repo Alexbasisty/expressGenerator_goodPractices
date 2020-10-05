19576051e7d04873af4f0d75ffe2c09e21a41bae
Alex Basystyi <alexbasisty@gmail.com>
2020-10-05 20:00:43 +0200

    app .js, delete & create

--- app.js
+++ app.js
@@ -1,41 +1,30 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express from 'express';
+import expressRateLimit from 'express-rate-limit';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
+let user = {
+  name: 'Alex',
+  modified: new Date()
+}
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.param('name', (req, res, next, name) => {
+  req.name = name;
+  next()
+})
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.get('/user', (req, res) => {
+  res.append('Last-Modified', user.modified.toDateString())
+  res.json(user)
+})
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.put('user/:name', (req, res) => {
+  user.name = req.name;
+  user.modified = new Date()
+  res.append('Last-Modified', user.modified.toDateString())
+  res.json(user)
+})
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+app.listen(3010, () => {
+  console.log('Listening on 3010')
+})
